3ca261254375d05b5a65e451f8c6eaa4eb48aa5e
Gabriel P. B. Piazza <pedro.piazza99@gmail.com>
2025-03-28 08:52:44 -0300

    feat: implement parcially parse streaming data

--- httpfromtcp/internal/request/request.go
+++ httpfromtcp/internal/request/request.go
@@ -1,1 +1,211 @@
 package request
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+	"unicode"
+)
+
+const (
+	// carrieage return = \r
+	// line feed = \n
+	// carrieage return + line feed = CL
+	lineBreak = "\r\n"
+
+	// single space = SP
+	space    = " "
+	buffSize = 8
+
+	httpVersionSuported = "HTTP/1.1"
+
+	MethodGet     = "GET"
+	MethodHead    = "HEAD"
+	MethodPost    = "POST"
+	MethodPut     = "PUT"
+	MethodPatch   = "PATCH"
+	MethodDelete  = "DELETE"
+	MethodConnect = "CONNECT"
+	MethodOptions = "OPTIONS"
+	MethodTrace   = "TRACE"
+)
+
+var (
+	lineBreakBytes = []byte(lineBreak)
+	spaceBytes     = []byte(space)
+
+	AllMethods = []string{
+		MethodGet,
+		MethodHead,
+		MethodPost,
+		MethodPut,
+		MethodPatch,
+		MethodDelete,
+		MethodConnect,
+		MethodOptions,
+		MethodTrace,
+	}
+)
+
+type Request struct {
+	RequestLine RequestLine
+
+	// isFullParsed holds the state of a request
+	// while isFullParsed is false the request dint finish to parse yet
+	isFullParsed bool
+
+	rawRequest string
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	request := new(Request)
+	var bytesReaded int
+	var bytesParsed int
+
+	buff := make([]byte, buffSize)
+	for !request.isFullParsed {
+		nFromReader, err := reader.Read(buff)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		buff = buff[:nFromReader]
+
+		newBuff := make([]byte, 2*len(buff))
+		if len(buff) == cap(buff) {
+			_ = copy(newBuff, buff)
+		}
+
+		bytesReaded += nFromReader
+		n, err := request.parse(buff)
+		if err != nil {
+			return nil, err
+		}
+		bytesParsed += n
+	}
+
+	return request, nil
+}
+
+func isAllCaps(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) {
+			return false
+		}
+		if !unicode.IsUpper(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func (r *Request) parse(data []byte) (int, error) {
+	if r.isFullParsed {
+		return 0, errors.New("error: trying to parse data in a done state")
+	}
+
+	n, err := r.parseRequestLine(data)
+	if err != nil {
+		return 0, err
+	}
+
+	return n, nil
+}
+
+// TODO: this implementation should parse data as
+// the data come from and not store the all data it self and only parse when has full data in
+func (r *Request) parseRequestLine(data []byte) (int, error) {
+	dataStr := string(data)
+	r.rawRequest += dataStr
+
+	if !strings.Contains(r.rawRequest, lineBreak) {
+		return 0, nil
+	}
+
+	requestPerLine := strings.Split(r.rawRequest, lineBreak)
+
+	requestLine := requestPerLine[0]
+
+	requestLinePerSpace := strings.Split(requestLine, space)
+
+	if len(requestLinePerSpace) != 3 {
+		return 0, errors.New("request line has not 3 parts format")
+	}
+
+	method := requestLinePerSpace[0]
+	target := requestLinePerSpace[1]
+	fullHttpV := requestLinePerSpace[2]
+
+	httpVSplited := strings.Split(fullHttpV, "/")
+
+	if err := r.validateMethod(method); err != nil {
+		return 0, err
+	}
+
+	if err := r.validateTarget(target); err != nil {
+		return 0, err
+	}
+
+	if err := r.validateHTTPVersion(fullHttpV, httpVSplited); err != nil {
+		return 0, err
+	}
+
+	httpV := httpVSplited[1]
+
+	r.RequestLine.HttpVersion = httpV
+	r.RequestLine.RequestTarget = target
+	r.RequestLine.Method = method
+
+	return len(requestLine), nil
+}
+
+func (r *Request) validateMethod(method string) error {
+	if !isAllCaps(method) {
+		return errors.New("request method malformed method is not in all captal letter")
+	}
+
+	for _, mappedM := range AllMethods {
+		if mappedM == method {
+			return nil
+		}
+	}
+
+	return fmt.Errorf(
+		"request method unsported - method got %s - see AllMethods variable to suported methods",
+		method,
+	)
+}
+
+func (r *Request) validateTarget(target string) error {
+	return nil
+}
+
+func (r *Request) validateHTTPVersion(httpV string, httpVSplited []string) error {
+	// TODO: add valiadtion to ensure version is digit . digit
+	// now i am not looking if the version is a valid version
+
+	if len(httpVSplited) != 2 {
+		return errors.New("malformed http version expected <HTTP-NAME>/<digit>.<digit>")
+	}
+
+	if httpV != httpVersionSuported {
+		return fmt.Errorf(
+			"unsoported http version - the httpVersion is %s and only httpVersion suported is %s",
+			httpV,
+			httpVersionSuported,
+		)
+	}
+
+	return nil
+}
--- httpfromtcp/internal/request/request_test.go
+++ httpfromtcp/internal/request/request_test.go
@@ -1,11 +1,160 @@
 package request
 
 import (
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "gremio", "gremio")
+	t.Run("Good request line without request target method Get", func(t *testing.T) {
+		reader := &chunkReader{
+			data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+			numBytesPerRead: 1,
+		}
+
+		r, err := RequestFromReader(reader)
+		require.NoError(t, err)
+		require.NotNil(t, r)
+		assert.Equal(t, "GET", r.RequestLine.Method)
+		assert.Equal(t, "/", r.RequestLine.RequestTarget)
+		assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+	})
+
+	t.Run("Good request line with request targer", func(t *testing.T) {
+		reader := &chunkReader{
+			data:            "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+			numBytesPerRead: 1000,
+		}
+
+		r, err := RequestFromReader(reader)
+
+		require.NoError(t, err)
+		require.NotNil(t, r)
+		assert.Equal(t, "GET", r.RequestLine.Method)
+		assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+		assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+	})
+
+	t.Run("request line without http method", func(t *testing.T) {
+		reader := &chunkReader{
+			data:            "/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+			numBytesPerRead: 200,
+		}
+
+		r, err := RequestFromReader(reader)
+
+		require.Nil(t, r)
+		require.Error(t, err)
+		require.ErrorContains(t, err, "request line has not 3 parts format")
+	})
+
+	t.Run("request line without request targe", func(t *testing.T) {
+		reader := &chunkReader{
+			data:            "GET HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+			numBytesPerRead: 8,
+		}
+
+		r, err := RequestFromReader(reader)
+
+		require.Nil(t, r)
+		require.Error(t, err)
+		require.ErrorContains(t, err, "request line has not 3 parts format")
+	})
+
+	t.Run("request line without http version targe", func(t *testing.T) {
+		reader := &chunkReader{
+			data:            "GET /coffe\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+			numBytesPerRead: 8,
+		}
+
+		r, err := RequestFromReader(reader)
+
+		require.Nil(t, r)
+		require.Error(t, err)
+		require.ErrorContains(t, err, "request line has not 3 parts format")
+	})
+
+	t.Run("request line with method malformed", func(t *testing.T) {
+		reader := &chunkReader{
+			data:            "get / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+			numBytesPerRead: 8,
+		}
+
+		r, err := RequestFromReader(reader)
+
+		require.Nil(t, r)
+		require.Error(t, err)
+		require.ErrorContains(t, err, "request method malformed method is not in all captal letter")
+	})
+
+	t.Run("request line with method not mapped", func(t *testing.T) {
+		reader := &chunkReader{
+			data:            "PIZZA / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+			numBytesPerRead: 8,
+		}
+
+		r, err := RequestFromReader(reader)
+
+		require.Nil(t, r)
+		require.Error(t, err)
+		require.ErrorContains(t, err, "request method unsported - method got PIZZA - see AllMethods variable to suported methods")
+	})
+
+	t.Run("request line with http version not suported", func(t *testing.T) {
+		reader := &chunkReader{
+			data:            "GET / HTTP/2.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+			numBytesPerRead: 8,
+		}
+
+		r, err := RequestFromReader(reader)
+
+		require.Nil(t, r)
+		require.Error(t, err)
+		require.ErrorContains(t, err, "unsoported http version - the httpVersion is HTTP/2.0 and only httpVersion suported is HTTP/1.1")
+	})
+
+	t.Run("request line with http version malformed", func(t *testing.T) {
+		reader := &chunkReader{
+			data:            "GET / HTTP2.0\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+			numBytesPerRead: 8,
+		}
+
+		r, err := RequestFromReader(reader)
+
+		require.Nil(t, r)
+		require.Error(t, err)
+		require.ErrorContains(t, err, "malformed http version expected <HTTP-NAME>/<digit>.<digit>")
+	})
+}
+
+type chunkReader struct {
+	data            string
+	numBytesPerRead int
+	pos             int
+}
+
+// Read reads up to len(p) or numBytesPerRead bytes from the string per call
+// its useful for simulating reading a variable number of bytes per chunk from a network connection
+func (cr *chunkReader) Read(p []byte) (n int, err error) {
+	if cr.pos >= len(cr.data) {
+		return 0, io.EOF
+	}
+
+	endIndex := cr.pos + cr.numBytesPerRead
+	if endIndex > len(cr.data) {
+		endIndex = len(cr.data)
+	}
+
+	n = copy(p, cr.data[cr.pos:endIndex])
+
+	cr.pos += n
+	if n > cr.numBytesPerRead {
+		n = cr.numBytesPerRead
+		cr.pos -= n - cr.numBytesPerRead
+	}
+
+	return n, nil
 }
